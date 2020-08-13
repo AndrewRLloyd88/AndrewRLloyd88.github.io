8008021b64a2b2829bcb1e7e0c564cd0351c0176
Andrew Lloyd <andrew.lloyd@googlemail.com>
2020-08-13 11:46:01 -0700

    Updates

---
+++ precache-manifest.193f8a5faf890badc7ade07310630919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603737af629e0222515f913b3a84d27e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84de84a780f89e82158b",
+    "url": "/static/css/main.f452a756.chunk.css"
+  },
+  {
+    "revision": "48e9af22a4c5553defbe",
+    "url": "/static/js/2.41e59e31.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.41e59e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84de84a780f89e82158b",
+    "url": "/static/js/main.671a0f8f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
