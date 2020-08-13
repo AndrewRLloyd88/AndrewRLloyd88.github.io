57b4f84ecde4150c2451bb13e7ccf8625bf68577
Andrew Lloyd <andrew.lloyd@googlemail.com>
2020-08-13 11:31:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ScheduleIcon from '@material-ui/icons/Schedule';
 
 function App() {
+  const [activity, setActivity] = useState(null);
+  const apiURL = 'https://www.boredapi.com/api/activity';
+  const [butText, setButText] = useState("Fetch a random activity")
+  const d = new Date().getFullYear(); 
+  
+
+  function fetchActivity() {
+    fetch(apiURL)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data.activity)
+       setActivity(data.activity)
+       setButText("Fetch a different activity");
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div className="rose">
+      <h2><ScheduleIcon /> Random Activity Generator</h2>
     </div>
-  );
+    <div className="flex-row">
+      <h2>Press the button to generate a random activity to do to cure the bordeom!</h2>
+    </div>
+    <div className="flex-row">
+      <button className="fetch-button" onClick={fetchActivity}>{butText}</button>
+    </div>
+    
+
+    
+   
+      <div className="activities flex-row">
+    <h1>{activity}</h1>   
+    </div>
+
+    <footer>
+    
+      <p>Copyright © Andrew Lloyd {d}</p>
+    </footer>
+    </div>
+  )
+
 }
 
 export default App;
